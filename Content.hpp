31f962f419893a8b6fc4f762ca99fd75b9f936bf
WERY Alexandre <w15001355@A-F001925-L.salsa.univ-amu.fr>
2017-09-11 09:48:31 +0200

    add operator + support to meta::Content

--- Content.hpp
+++ Content.hpp
@@ -9,23 +9,30 @@ namespace meta
             Size = sizeof... (str)
         };
         typedef T_t (res)[Size+1];
+
         static constexpr const res &get()
         { return data; }
+
         template<typename Type_t>
         static constexpr Type_t agregat()
         { return Type_t{data}; }
+
         static constexpr res data = {str..., T_t(0)};
-        template<typename Out, typename Function>
-        static constexpr Out Apply(Function &f)
-        {
-            return f(str...);
-        }
-        constexpr operator const T_t *() const
+
+        constexpr operator const T_t *() const noexcept
         {
             return data;
+        }
+
+        template<T_t... str2>
+        constexpr Content<T_t, str..., str2...> operator+(Content<T_t, str2...>) noexcept
+        {
+            return Content<T_t, str..., str2...>{};
         }
     };
 
     template<typename T_t, T_t... str>
     constexpr typename Content<T_t, str...>::res Content<T_t, str...>::data;
+
 }
+
